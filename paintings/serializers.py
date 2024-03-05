15b05d7d9954f702f540fa2cec2e371fee38f7a0
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-03-05 21:26:47 +0100

    feat: add validation to the image sizes in PaintingSerializer

--- paintings/serializers.py
+++ paintings/serializers.py
@@ -10,6 +10,18 @@ class PaintingSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     orientation = serializers.SerializerMethodField()
 
+    def validate_image(self, value):
+        if value.size > 1024 * 1024 * 2:
+            raise serializers.ValidationError('Image size is larger than 2MB!')
+        if value.image.width > 4096:
+            raise serializers.ValidationError('Image width is larger than '
+                                              '4096px!')
+        if value.image.height > 4096:
+            raise serializers.ValidationError('Image height is larger than '
+                                              '4096px!')
+        return value
+
+
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
