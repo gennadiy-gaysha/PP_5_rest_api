393e6be840541bf1839ffc4f14b7df72ca811766
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-03-09 17:55:27 +0100

    feat: add filter for Faves, Watchlist and User's paintings

--- paintings/filters.py
+++ paintings/filters.py
@@ -2,6 +2,7 @@ from django_filters import rest_framework as df_filters, ChoiceFilter
 from .models import Painting
 from django.db.models import F
 
+
 class PaintingFilter(df_filters.FilterSet):
     ORIENTATION_CHOICES = [
         ('Landscape', 'Landscape'),
@@ -11,16 +12,23 @@ class PaintingFilter(df_filters.FilterSet):
 
     orientation = ChoiceFilter(choices=ORIENTATION_CHOICES,
                                method='filter_orientation')
+
     class Meta:
         model = Painting
-        fields = ['theme', 'technique', 'availability']
+        fields = ['theme', 'technique', 'availability',
+                  # User's Faves
+                  'owner__followed__owner__profile',
+                  # User's Watchlist
+                  'observations__owner__profile',
+                  # User's Paintings
+                  'owner__profile']
 
     def filter_orientation(self, queryset, name, value):
         """
         Filters the queryset based on painting orientation.
         Assumes 'Landscape', 'Portrait', and 'Square' as valid values for 'value'.
         """
-        if value == 'Landscape':
+        if value.lower() == 'Landscape':
             return queryset.filter(width__gt=F('height'))
         elif value.lower() == 'portrait':
             return queryset.filter(width__lt=F('height'))
--- paintings/views.py
+++ paintings/views.py
@@ -9,20 +9,20 @@ from .filters import PaintingFilter
 
 class PaintingList(generics.ListCreateAPIView):
     serializer_class = PaintingSerializer
-
-    # queryset = Painting.objects.all()
     queryset = Painting.objects.annotate(
         comments_count=Count('comment', distinct=True),
         observations_count=Count('observations', distinct=True)
     ).order_by('-created_at')
 
-    filter_backends = [filters.OrderingFilter, filters.SearchFilter, df_filters.DjangoFilterBackend]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter,
+                       df_filters.DjangoFilterBackend]
 
     filterset_class = PaintingFilter
     search_fields = ['owner__profile__name', 'title']
     ordering_fields = ['observations_count', 'comments_count',
                        'observations__created_at', 'price']
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
