e75a18451cb89bfaf459db7ff37127a8ca6bd654
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-03-09 18:34:35 +0100

    feat: add filter to retrieve all the comments for a given painting

--- comments/views.py
+++ comments/views.py
@@ -1,15 +1,24 @@
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
-from .serializers import CommentSerializer,CommentDetailSerializer
+from .serializers import CommentSerializer, CommentDetailSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class CommentList(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
 
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = [
+        # retrieves all the comments associated with a given painting
+        'painting'
+    ]
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = CommentDetailSerializer
