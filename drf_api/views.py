0b76a6cfc73fc92616c73fe26e88fd2278e85161
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-03-10 17:44:11 +0100

    feat: add heroku endpoints to the main api url

--- drf_api/views.py
+++ drf_api/views.py
@@ -9,4 +9,9 @@ def endpoint_list(request):
         'http://127.0.0.1:8000/comments/',
         'http://127.0.0.1:8000/observations/',
         'http://127.0.0.1:8000/followers/',
+        'https://drf-api-app-gaysha-repeat-150999686cdd.herokuapp.com/profiles/',
+        'https://drf-api-app-gaysha-repeat-150999686cdd.herokuapp.com/posts/',
+        'https://drf-api-app-gaysha-repeat-150999686cdd.herokuapp.com/comments/',
+        'https://drf-api-app-gaysha-repeat-150999686cdd.herokuapp.com/likes/',
+        'https://drf-api-app-gaysha-repeat-150999686cdd.herokuapp.com/followers/',
     ])
