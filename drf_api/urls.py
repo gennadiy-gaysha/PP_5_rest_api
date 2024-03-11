7366fb9aa8215764442bdb6137b645e9e78aaedc
Gennadiy Gaysha <gennadiy.gaysha@gmail.com>
2024-03-11 15:41:14 +0100

    fix: dj-rest-auth Bug Fix

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import endpoint_list
+from .views import endpoint_list, logout_route
 
 urlpatterns = [
     path('', endpoint_list),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+
+    path('dj-rest-auth/logout/', logout_route),
+
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
     path('dj-rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
--- drf_api/views.py
+++ drf_api/views.py
@@ -1,5 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .settings import (
+    JWT_AUTH_COOKIE,
+    JWT_AUTH_REFRESH_COOKIE,
+    JWT_AUTH_SAMESITE,
+    JWT_AUTH_SECURE
+)
+
 
 @api_view()
 def endpoint_list(request):
@@ -16,3 +23,27 @@ def endpoint_list(request):
         'https://pp-5-drf-api-cb9dad6bdfdf.herokuapp.com/followers/',
     ])
 
+
+# dj-rest-auth logout view fix
+@api_view(['POST'])
+def logout_route(request):
+    response = Response()
+    response.set_cookie(
+        key=JWT_AUTH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    response.set_cookie(
+        key=JWT_AUTH_REFRESH_COOKIE,
+        value='',
+        httponly=True,
+        expires='Thu, 01 Jan 1970 00:00:00 GMT',
+        max_age=0,
+        samesite=JWT_AUTH_SAMESITE,
+        secure=JWT_AUTH_SECURE,
+    )
+    return response
